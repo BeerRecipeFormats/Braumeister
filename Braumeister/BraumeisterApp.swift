dfa1e30160fc1724b43c20fcccc23edfd3b5b63c
Thomas Bonk <thomas@meandmymac.de>
2022-04-14 06:38:45 +0200

    Initial Commit

---
+++ Braumeister/BraumeisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BraumeisterApp.swift
+//  Braumeister
+//
+//  Created by Thomas Bonk on 14.04.22.
+//
+
+import SwiftUI
+
+@main
+struct BraumeisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
